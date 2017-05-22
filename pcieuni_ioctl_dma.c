acbb5d69443321beff744ad6999a8d14aab5b9fb
Martin Killenberg <martin.killenberg@desy.de>
2017-05-22 09:17:00 +0200

    debug counters are only active when compiled with debug flag

--- pcieuni_ioctl_dma.c
+++ pcieuni_ioctl_dma.c
@@ -11,7 +11,9 @@
 #include "pcieuni_fnc.h"
 #include <gpcieuni/pcieuni_buffer.h>
 
+#ifdef PCIEUNI_DEBUG
 static atomic_t dma_request_counter = ATOMIC_INIT(0);  
+#endif
 
 /**
  * @brief Initiates DMA read from device
@@ -34,9 +36,11 @@ int pcieuni_start_dma_read(pcieuni_dev* dev, pcieuni_buffer *targetBuffer)
     struct module_dev *mdev = pcieuni_get_mdev(dev);
     int retVal = 0;
 
+#ifdef PCIEUNI_DEBUG
     atomic_inc(&dma_request_counter);
     
     PDEBUG(dev->name, "pcieuni_start_dma_read(offset=0x%lx, maxSize=0x%lx) number %i\n", targetBuffer->dma_offset, targetBuffer->dma_size, atomic_read(&dma_request_counter)); 
+#endif /*PCIEUNI_DEBUG*/
 
     // reserve device registers IO
     retVal = pcieuni_dma_reserve(mdev, targetBuffer);
